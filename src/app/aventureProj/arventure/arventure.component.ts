8ced87229b36e99fb582e17aa0708e24f4848132
chform2 <94845732+chform2@users.noreply.github.com>
2022-01-06 12:55:02 +0100

    create conponent card final

--- src/app/aventureProj/arventure/arventure.component.ts
+++ src/app/aventureProj/arventure/arventure.component.ts
@@ -9,13 +9,6 @@ import { ArventureFeature } from 'src/app/class/ArventureFeature';
 })
 export class ArventureComponent implements OnInit {
 arventure:ArventureFeature;
-   id:string;
-   cardList:any[]= [
-      {id: 1, img: 'tempete_ok.webp', title: 'Tempête', alt: 'tempete'},
-      {id: 2, img: 'moutains_ok.webp', title: 'Montagne', alt: 'montagne'},
-      {id: 3, img: 'forestGood.webp', title: 'Forêt', alt: 'foret'},
-      {id: 3, img: 'refuge_ok.webp', title: 'Refuge', alt: 'refuge'}
-   ];
 
   constructor(private route:Router) {
      this.arventure = new ArventureFeature();
@@ -25,15 +18,4 @@ arventure:ArventureFeature;
     this.arventure.start();
   }
   
-  isCollideOk(){
-    if(localStorage.getItem("toto") == "1"){
-
-      this.id = this.arventure.idCarteCollide();
-      console.log("CArteId", this.id);
-      
-      this.route.navigateByUrl(`/genererHistoire/${this.id}`);
-    }
-    localStorage.setItem("toto", "0");
-  }
-  
-}
+} 
